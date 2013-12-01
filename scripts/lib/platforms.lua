40786ade372f5c739c5d84667c4754daac6091f8
Ben Wilhelm <zorba-git20110501@pavlovian.net>
2013-12-01 02:18:56 -0800

    Build msvc debug versions as well as release versions.

--- scripts/lib/platforms.lua
+++ scripts/lib/platforms.lua
@@ -2,19 +2,19 @@
 projects = {
   msvc9 = {
     generator = "vs2008",
-    build = [[tmp= temp= cmd /Q /C call "c:/Program Files (x86)/Microsoft Visual Studio 9.0/Common7/Tools/vsvars32.bat" "&&" msbuild Frames.sln /p:configuration=Release /m]],
+    build = [[tmp= temp= cmd /Q /C call "c:/Program Files (x86)/Microsoft Visual Studio 9.0/Common7/Tools/vsvars32.bat" "&&" msbuild Frames.sln /p:configuration=Release /m && msbuild Frames.sln /p:configuration=Debug /m]],
   },
   msvc10 = {
     generator = "vs2010",
-    build = [[cmd /Q /C call "c:/Program Files (x86)/Microsoft Visual Studio 10.0/Common7/Tools/vsvars32.bat" "&&" msbuild Frames.sln /p:configuration=Release /m]],
+    build = [[cmd /Q /C call "c:/Program Files (x86)/Microsoft Visual Studio 10.0/Common7/Tools/vsvars32.bat" "&&" msbuild Frames.sln /p:configuration=Release /m && msbuild Frames.sln /p:configuration=Debug /m]],
   },
   msvc11 = {
     generator = "vs2012",
-    build = [[cmd /Q /C call "c:/Program Files (x86)/Microsoft Visual Studio 11.0/Common7/Tools/vsvars32.bat" "&&" msbuild Frames.sln /p:configuration=Release /m]],
+    build = [[cmd /Q /C call "c:/Program Files (x86)/Microsoft Visual Studio 11.0/Common7/Tools/vsvars32.bat" "&&" msbuild Frames.sln /p:configuration=Release /m && msbuild Frames.sln /p:configuration=Debug /m]],
   },
   msvc12 = {
     generator = "vs2013",
-    build = [[cmd /Q /C call "c:/Program Files (x86)/Microsoft Visual Studio 12.0/Common7/Tools/vsvars32.bat" "&&" msbuild Frames.sln /p:configuration=Release /m]],
+    build = [[cmd /Q /C call "c:/Program Files (x86)/Microsoft Visual Studio 12.0/Common7/Tools/vsvars32.bat" "&&" msbuild Frames.sln /p:configuration=Release /m && msbuild Frames.sln /p:configuration=Debug /m]],
   },
   mingw = {
     generator = "gmake",
