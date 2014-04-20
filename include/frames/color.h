8aba6c907e6a5f372d43b1986b01b1892bc57a8c
Ben Rog-Wilhelm <zorba-git-ed209@pavlovian.net>
2014-04-20 07:44:33 -0700

    Documentation for Color.

--- include/frames/color.h
+++ include/frames/color.h
@@ -1,15 +1,27 @@
-// The Color class
-
 #ifndef FRAMES_COLOR
 #define FRAMES_COLOR
 
 namespace Frames {
+  /// Stores a color value.
+  /**
+  Stores a standard RGBA color value with transparency. Each value is on a 0-to-1 scale, where 1 is fully lit or fully opaque.
+  */
   class Color {
   public:
     Color() : r(0), g(0), b(0), a(1) { };
     Color(float r, float g, float b, float a = 1.f) : r(r), g(g), b(b), a(a) { };
     union {
-      struct { float r, g, b, a; };
+      struct {
+        /// Red color channel.
+        float r;
+        /// Green color channel.
+        float g;
+        /// Blue color channel.
+        float b;
+        /// Alpha color channel. 0 is transparent, 1 is opaque.
+        float a;
+      };
+      /// Array representation, in RGBA order. Can be used interchangably with the other members.
       float c[4];
     };
   };
