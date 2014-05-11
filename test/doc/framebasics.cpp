e4297bfb78937c5e005d92d909698445b2cb2efd
Ben Wilhelm <zorba-git20110501@pavlovian.net>
2014-05-11 05:09:53 -0700

    Final image for Frame Basics.

--- test/doc/framebasics.cpp
+++ test/doc/framebasics.cpp
@@ -62,10 +62,10 @@ TEST(Framebasics, Example) {
   TestSnapshot(env, "ref/doc/framebasics_example");
 };
 
-void LayerMake(Frames::Layout *root, float width, float height, float x, float y, float layer, Frames::Color color, float text_y) {
+void LayerMake(Frames::Layout *root, float x, float y, float layer, Frames::Color color, float text_y) {
   Frames::Frame *frame = Frames::Frame::Create(root, "layer");
-  frame->WidthSet(width);
-  frame->HeightSet(height);
+  frame->WidthSet(80);
+  frame->HeightSet(80);
   frame->PinSet(Frames::TOPLEFT, root, Frames::TOPLEFT, x, y);
   frame->BackgroundSet(color);
   frame->LayerSet(layer);
@@ -80,32 +80,61 @@ void LayerMake(Frames::Layout *root, float width, float height, float x, float y
 TEST(Framebasics, Layering) {
   TestEnvironment env(true, 640, 360);
 
-  LayerMake(env->RootGet(), 80, 80, 40, 40, 0, tdc::blue, 0);
-  LayerMake(env->RootGet(), 80, 80, 60, 60, 1, tdc::orange, 0);
-  LayerMake(env->RootGet(), 80, 80, 80, 80, 2, tdc::red, 0);
+  LayerMake(env->RootGet(), 40, 40, 0, tdc::blue, 0);
+  LayerMake(env->RootGet(), 60, 60, 1, tdc::orange, 0);
+  LayerMake(env->RootGet(), 80, 80, 2, tdc::red, 0);
 
-  LayerMake(env->RootGet(), 80, 80, 220, 40, -1, tdc::purple, 0);
-  LayerMake(env->RootGet(), 80, 80, 280, 60, 0, tdc::red, 0);
-  LayerMake(env->RootGet(), 80, 80, 240, 80, 1, tdc::cyan, 0);
+  LayerMake(env->RootGet(), 220, 40, -1, tdc::purple, 0);
+  LayerMake(env->RootGet(), 280, 60, 0, tdc::red, 0);
+  LayerMake(env->RootGet(), 240, 80, 1, tdc::cyan, 0);
 
-  LayerMake(env->RootGet(), 80, 80, 460, 80, 0, tdc::red, 1);
-  LayerMake(env->RootGet(), 80, 80, 420, 60, 0.5, tdc::orange, 1);
-  LayerMake(env->RootGet(), 80, 80, 440, 40, 20, tdc::green, 1);
+  LayerMake(env->RootGet(), 460, 80, 0, tdc::red, 1);
+  LayerMake(env->RootGet(), 420, 60, 0.5, tdc::orange, 1);
+  LayerMake(env->RootGet(), 440, 40, 20, tdc::green, 1);
 
-  LayerMake(env->RootGet(), 80, 80, 40, 260, -1000, tdc::red, 1);
-  LayerMake(env->RootGet(), 80, 80, 60, 240, -0.2f, tdc::orange, 1);
-  LayerMake(env->RootGet(), 80, 80, 80, 220, 0.01f, tdc::green, 1);
-  LayerMake(env->RootGet(), 80, 80, 100, 200, 0.015f, tdc::cyan, 1);
+  LayerMake(env->RootGet(), 40, 260, -1000, tdc::red, 1);
+  LayerMake(env->RootGet(), 60, 240, -0.2f, tdc::orange, 1);
+  LayerMake(env->RootGet(), 80, 220, 0.01f, tdc::green, 1);
+  LayerMake(env->RootGet(), 100, 200, 0.015f, tdc::cyan, 1);
 
-  LayerMake(env->RootGet(), 80, 80, 240, 200, 9000, tdc::cyan, 0);
-  LayerMake(env->RootGet(), 80, 80, 220, 220, 9001, tdc::green, 0);
-  LayerMake(env->RootGet(), 80, 80, 280, 240, 9002, tdc::purple, 0);
-  LayerMake(env->RootGet(), 80, 80, 260, 260, 9003, tdc::blue, 0);
+  LayerMake(env->RootGet(), 240, 200, 9000, tdc::cyan, 0);
+  LayerMake(env->RootGet(), 220, 220, 9001, tdc::green, 0);
+  LayerMake(env->RootGet(), 280, 240, 9002, tdc::purple, 0);
+  LayerMake(env->RootGet(), 260, 260, 9003, tdc::blue, 0);
 
-  LayerMake(env->RootGet(), 80, 80, 440, 260, 42, tdc::orange, 1);
-  LayerMake(env->RootGet(), 80, 80, 460, 240, 113, tdc::red, 1);
-  LayerMake(env->RootGet(), 80, 80, 420, 220, 1024, tdc::cyan, 1);
-  LayerMake(env->RootGet(), 80, 80, 480, 200, 2600, tdc::purple, 1);
+  LayerMake(env->RootGet(), 440, 260, 42, tdc::orange, 1);
+  LayerMake(env->RootGet(), 460, 240, 113, tdc::red, 1);
+  LayerMake(env->RootGet(), 420, 220, 1024, tdc::cyan, 1);
+  LayerMake(env->RootGet(), 480, 200, 2600, tdc::purple, 1);
 
   TestSnapshot(env, "ref/doc/framebasics_layering");
+};
+
+Frames::Frame *HierarchMake(Frames::Layout *root, Frames::Layout *parent, float x, float y, float layer, Frames::Color color, const std::string &intex) {
+  Frames::Frame *frame = Frames::Frame::Create(parent, "layer");
+  frame->WidthSet(200);
+  frame->HeightSet(200);
+  frame->PinSet(Frames::TOPLEFT, root, Frames::TOPLEFT, x, y);
+  frame->BackgroundSet(color);
+  frame->LayerSet(layer);
+
+  Frames::Text *text = Frames::Text::Create(frame, "text");
+  text->PinSet(Frames::TOPLEFT, frame, Frames::TOPLEFT, 3, 3);
+  text->SizeSet(12);
+  text->FontSet("geo_1.ttf");
+  text->TextSet(Frames::detail::Format("%s\nLayer %g", intex, layer));
+
+  return frame;
+}
+
+TEST(Framebasics, Hierarchy) {
+  TestEnvironment env(true, 640, 360);
+
+  Frames::Frame *rl = HierarchMake(env->RootGet(), env->RootGet(), 100, 40, 0, tdc::red, "RED");
+  Frames::Frame *rr = HierarchMake(env->RootGet(), env->RootGet(), 340, 50, 1, tdc::blue, "BLUE");
+
+  HierarchMake(env->RootGet(), rl, 130, 100, 100, tdc::orange, "ORANGE - Child of RED");
+  HierarchMake(env->RootGet(), rr, 320, 140, 0, tdc::purple, "PURPLE - Child of BLUE");
+
+  TestSnapshot(env, "ref/doc/framebasics_hierarchy");
 };
