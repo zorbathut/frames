b2bf56f6b11fa70e81ea1e34c681c88335ba66ac
Ben Wilhelm <zorba-git20110501@pavlovian.net>
2014-06-07 02:17:49 -0700

    Remove some unnecessary opengl _EXT suffixes.

--- src/opengl/renderer_opengl.cpp
+++ src/opengl/renderer_opengl.cpp
@@ -65,8 +65,8 @@ namespace Frames {
       glBindTexture(GL_TEXTURE_2D, m_id);
       glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
       glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE_EXT);
-      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE_EXT);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 
       glBindTexture(GL_TEXTURE_2D, m_id);
       glTexImage2D(GL_TEXTURE_2D, 0, input_tex_mode, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, 0); // I'm assuming the last three values are irrelevant
