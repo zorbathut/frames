35cb2be9854af34e2a51d487f9ebafac0498f7db
Ben Wilhelm <zorba-git20110501@pavlovian.net>
2014-06-06 22:10:46 -0700

    Prevent test machines from turning off while running tests.

--- test/main.cpp
+++ test/main.cpp
@@ -3,10 +3,13 @@
 
 #include <gtest/gtest.h>
 
+#include <windows.h>
+
 #undef main // dammit, sdl
 
 int main(int argc, char **argv) {
   testing::InitGoogleTest(&argc, argv);
+  SetThreadExecutionState(ES_CONTINUOUS | ES_SYSTEM_REQUIRED);  // This isn't platform-agnostic, but it's good enough for now
   if (argc > 1) {
     RendererIdSet(argv[1]);
   }
