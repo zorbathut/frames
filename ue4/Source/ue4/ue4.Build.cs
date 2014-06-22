6213323f96f1a31246ad3251c62a3172d7f51ca1
Ben Wilhelm <zorba-git20110501@pavlovian.net>
2014-06-22 13:48:12 -0700

    Get UE4 building properly, at least with some sets of build parameters.

--- ue4/Source/ue4/ue4.Build.cs
+++ ue4/Source/ue4/ue4.Build.cs
@@ -29,10 +29,19 @@ public class ue4 : ModuleRules
     string PlatformString = (Target.Platform == UnrealTargetPlatform.Win64) ? "x64" : "x86";
     string PlatformStringFrames = (Target.Platform == UnrealTargetPlatform.Win64) ? "win64" : "win32";
     string DirectoryName = Path.GetDirectoryName( RulesCompiler.GetModuleFilename( this.GetType().Name ) );
-    string FramesBase = Path.Combine( DirectoryName, "..", "..", "..", ".." );
+    string FramesBase = Path.Combine( DirectoryName, "..", "..", ".." );
     
-    PublicAdditionalLibraries.Add(Path.Combine(FramesBase, "lib", "ue4_2", PlatformString, "frames.lib"));
-    PublicAdditionalLibraries.Add(Path.Combine(FramesBase, "lib", "ue4_2", PlatformString, "frames_dx11.lib"));
+    // choose appropriate libraries
+    if (Target.Configuration == UnrealTargetConfiguration.Debug || Target.Configuration == UnrealTargetConfiguration.DebugGame)
+    {
+      PublicAdditionalLibraries.Add(Path.Combine(FramesBase, "lib", "ue4_2", PlatformString, "framesd.lib"));
+      PublicAdditionalLibraries.Add(Path.Combine(FramesBase, "lib", "ue4_2", PlatformString, "frames_dx11d.lib"));
+    }
+    else
+    {
+      PublicAdditionalLibraries.Add(Path.Combine(FramesBase, "lib", "ue4_2", PlatformString, "frames.lib"));
+      PublicAdditionalLibraries.Add(Path.Combine(FramesBase, "lib", "ue4_2", PlatformString, "frames_dx11.lib"));
+    }
     
     PublicAdditionalLibraries.Add(Path.Combine(FramesBase, "deps", "jpeg-9", PlatformStringFrames, "lib", "jpeg.lib"));
     PublicAdditionalLibraries.Add("d3dcompiler.lib");
