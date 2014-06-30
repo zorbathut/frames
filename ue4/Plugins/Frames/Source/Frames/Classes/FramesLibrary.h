be87787064bef08f888a90631cb7542bfb2ba76c
Ben Wilhelm <zorba-git20110501@pavlovian.net>
2014-06-30 10:02:36 -0700

    Do a little dance to avoid UHT bugs.

--- ue4/Plugins/Frames/Source/Frames/Classes/FramesLibrary.h
+++ ue4/Plugins/Frames/Source/Frames/Classes/FramesLibrary.h
@@ -15,7 +15,8 @@
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with Frames.  If not, see <http://www.gnu.org/licenses/>. */
+    along with Frames.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #pragma once
 
